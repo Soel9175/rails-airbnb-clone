eaa2b54956f3d7c1f6c45a370d15a4a2de294b8c
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-17 09:23:50 +0000

    Made the validations a bit easier to read for Bookings model. Also added validation so that Booking.days > 0

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
-  validates :start_date, :days, :user_id, :item_id, presence: true
-  validates :days, numericality: { only_integer: true }
+  validates :start_date, presence: true
+  validates :days, presence: true
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :days, numericality: { only_integer: true, greater_than: 0 }
 end
