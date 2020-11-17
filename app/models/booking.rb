8f96b854a19ce27034d8288c28e7cf045859fd16
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-17 09:31:23 +0000

    The booking model was still referring to the 'days' field, so I changed it to use the 'end_date' field in line with the schema

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,8 @@
-require 'date'
-
 class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
-  validates :start_date, presence: true, numericality: { greater_than_or_equal_to: Date.today }
-  validates :end_date, presence: true, numericality: { greater_than_or_equal_to: :start_date }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
 end
