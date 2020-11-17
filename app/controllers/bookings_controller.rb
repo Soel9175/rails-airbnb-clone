cfcd59a0f600e2f98b694a6bb10279b87dbdd6f8
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-17 09:52:14 +0000

    Got a rough and ready thank you page created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,13 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @item = Item.find(params[:item_id])
   end
+
+  def create 
+    @booking = Booking.create(strong_params)
+    redirect_to pages_thankyou_path
+  end
+
+  def strong_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
