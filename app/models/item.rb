4db5992ded82c16e89b8649c0b9b6a2692456bfb
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-17 01:52:59 +0000

    Added left over validations for the Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :name, :size, :gender, :available, :price, :initial_condition, :brand, :num_uses, presence: true
+  validates :name, presence: true
+  validates :size, presence: true 
+  validates :gender, presence: true 
+  validates :available, presence: true
+  validates :price, presence: true, 
+            numericality: {greater_than_or_equal_to: 0, only_integer: false}, 
+            format: { with: /\A\d\.\d\d\z/, message: "price must have two decimal places" }
+  validates :initial_condition, presence: true
+  validates :brand, presence: true
+  validates :num_uses, presence: true, 
+            numericality: {greater_than_or_equal_to: 0, only_integer: true}
   validates :available, inclusion: { in: [true, false] }
 end
