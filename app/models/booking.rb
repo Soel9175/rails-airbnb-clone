99dd7d9c30174430fa6fb92a8735dddde6e51ca6
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-17 09:29:01 +0000

    Booking validations still had 'days' field, so i replaced it with the 'end_date' field to match the schema. Also added some validations for the dates to make sure they cant add a date in the past, or an end-date that is earlier than the start date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,10 @@
+require 'date'
+
 class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
-  validates :start_date, presence: true
-  validates :days, presence: true
+  validates :start_date, presence: true, numericality: { greater_than_or_equal_to: Date.today }
+  validates :end_date, presence: true, numericality: { greater_than_or_equal_to: :start_date }
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :days, numericality: { only_integer: true, greater_than: 0 }
 end
