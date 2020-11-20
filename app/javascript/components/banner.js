cd82dabe7adf926e4c8c7ae70f692aec56595ad3
Gareth Carter <gareth.carter@outlook.com>
2020-11-20 15:10:38 +0000

    Fixed navChange bug

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -14,10 +14,17 @@ const smoothScroll = () => {
 
 // function to change the colour of the navbar text for the brighter colour scheme on other pages
 const navChange = () => {
-  if (window.location.pathname != "/items") {
+  if ( (window.location.pathname != "/items") && !(window.location.pathname != "/") || (window.location.pathname != "/") && !(window.location.pathname != "/items") ) {
     const elements = document.getElementsByClassName("nav-link");
     const elements_array = Array.from(elements);
-    elements_array.forEach(element => element.style.color = "black");
+    elements_array.forEach(element => element.style.color = "#E0E1E1");
   };
 };
 export { smoothScroll, navChange };
+
+
+
+// if( (A && !B) || (B && !A) ) { ... }
+
+
+// if ( (window.location.pathname != "/items" && !window.location.pathname !="/") || (window.location.pathname != "/" && !window.location.pathname !="/items") )
